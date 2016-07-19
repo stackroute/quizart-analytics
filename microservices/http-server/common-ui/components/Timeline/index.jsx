da996a83dbee080f927f06387c0329cf798cd780
PreethiRadhakrishnan <preity_preethi@yahoo.com>
2016-07-19 10:31:31 +0530

    commit with add friend with uuid1

--- microservices/http-server/common-ui/components/Timeline/index.jsx
+++ microservices/http-server/common-ui/components/Timeline/index.jsx
@@ -1,103 +1,103 @@
-// import React from 'react';
-// import CommentForm from  './CommentForm';
-// import Tweet from 'react-tweet';
-// var socket = io('/tweets');
-// export default class Timeline extends React.Component{
-//   constructor (props){
-//      super(props);
-//      this.state = {tweets:[]};
-//
-//   }
-//
-// loadDataFromSever(){
-//   var id;
-//   if(this.props.user!=undefined){
-//     id ="user";
-//     console.log("====user timeline");
-//   }
-//  else {
-//     id ="SYTYCDGrandFinale";
-//    console.log("topic timeline");
-//  }
-//  var request =  $.ajax({
-//     url: "api/v1/timeline/twitter/getTwitterData/"+id,
-//     contentType: 'application/json',
-//     cache: false,
-//     headers: {JWT: localStorage.authToken}
-//
-//   });
-//
-//   request.done(function(data) {
-//                 console.log("=====id===",id);
-//                console.log("=======retrievedPosts=======",data);
-//                if(data instanceof Array){
-//                   console.log("=====tweets====",data);
-//                   this.setState({tweets:data})
-//                }
-//                else {
-//                   this.setState({tweets:data.statuses});
-//                }
-//
-//     }.bind(this));
-//
-//       request.fail(function(xhr, status, err) {
-//       console.error("api/v1/timeline/gettweet/", status, err.toString());
-//     }.bind(this));
-//
-//
-// }
-//
-//   componentDidMount(){
-//           this.loadDataFromSever();
-//           socket.emit("ctreatStream",{id:this.props.hashtag,localStorage.authToken})
-//           console.log("=====token",localStorage.authToken);
-//           var  that = this;
-//            socket.on('tweetData', function getTweet (tweet) {
-//            console.log("============tweet ",tweet);
-//           if(tweet.user.id!="undefined"){
-//              console.log("====username====",tweet.user.name);
-//              that.setState({tweets:[tweet].concat(that.state.tweets)});
-//          }
-// });
-//
-// }
-//
-//    handlePost(post){
-//
-//      console.log("====post=====",post);
-//      var request  =  $.ajax({
-//        url: "api/v1/timeline/twitter/postToTwitter",
-//        type: 'POST',
-//        data: JSON.stringify({text:post}),
-//        contentType: 'application/json',
-//        headers: {JWT: localStorage.authToken}
-//      });
-//
-//      request.done(function(data) {
-//      console.log("=========tweeted: "+JSON.stringify(data)+"========");
-//
-//        }.bind(this));
-//         request.fail( function(err) {
-//          console.error("api/v1/timeline/twitter/postToTwitter", status, err.toString());
-//        }.bind(this));
-//   }
-//
-//   render() {
-//
-//
-//      var createPost = this.state.tweets.map(function(data) {
-//
-//          return (<Tweet data = {data}/>);
-//     });
-//     return (
-//       <div>
-//         <CommentForm newPost = {this.handlePost.bind(this)}/>
-//         <div style = {{marginLeft:0}}>
-//             {
-//               createPost
-//             }
-//         </div>
-//       </div>
-//     )
-//   }
-// }
+import React from 'react';
+import CommentForm from  './CommentForm';
+import Tweet from 'react-tweet';
+var socket = io('/tweets');
+export default class Timeline extends React.Component{
+  constructor (props){
+     super(props);
+     this.state = {tweets:[]};
+
+  }
+
+loadDataFromSever(){
+  var id;
+  if(this.props.user!=undefined){
+    id ="user";
+    console.log("====user timeline");
+  }
+ else {
+    id ="SYTYCDGrandFinale";
+   console.log("topic timeline");
+ }
+ var request =  $.ajax({
+    url: "api/v1/timeline/twitter/getTwitterData/"+id,
+    contentType: 'application/json',
+    cache: false,
+    headers: {JWT: localStorage.authToken}
+
+  });
+
+  request.done(function(data) {
+                console.log("=====id===",id);
+               console.log("=======retrievedPosts=======",data);
+               if(data instanceof Array){
+                  console.log("=====tweets====",data);
+                  this.setState({tweets:data})
+               }
+               else {
+                  this.setState({tweets:data.statuses});
+               }
+
+    }.bind(this));
+
+      request.fail(function(xhr, status, err) {
+      console.error("api/v1/timeline/gettweet/", status, err.toString());
+    }.bind(this));
+
+
+}
+
+  componentDidMount(){
+          this.loadDataFromSever();
+          socket.emit("ctreatStream",{id:this.props.hashtag,localStorage.authToken})
+          console.log("=====token",localStorage.authToken);
+          var  that = this;
+           socket.on('tweetData', function getTweet (tweet) {
+           console.log("============tweet ",tweet);
+          if(tweet.user.id!="undefined"){
+             console.log("====username====",tweet.user.name);
+             that.setState({tweets:[tweet].concat(that.state.tweets)});
+         }
+});
+
+}
+
+   handlePost(post){
+
+     console.log("====post=====",post);
+     var request  =  $.ajax({
+       url: "api/v1/timeline/twitter/postToTwitter",
+       type: 'POST',
+       data: JSON.stringify({text:post}),
+       contentType: 'application/json',
+       headers: {JWT: localStorage.authToken}
+     });
+
+     request.done(function(data) {
+     console.log("=========tweeted: "+JSON.stringify(data)+"========");
+
+       }.bind(this));
+        request.fail( function(err) {
+         console.error("api/v1/timeline/twitter/postToTwitter", status, err.toString());
+       }.bind(this));
+  }
+
+  render() {
+
+
+     var createPost = this.state.tweets.map(function(data) {
+
+         return (<Tweet data = {data}/>);
+    });
+    return (
+      <div>
+        <CommentForm newPost = {this.handlePost.bind(this)}/>
+        <div style = {{marginLeft:0}}>
+            {
+              createPost
+            }
+        </div>
+      </div>
+    )
+  }
+}
