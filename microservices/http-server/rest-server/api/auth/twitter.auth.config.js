35f2da20ea41d8797daa4276874e8159369eebb2
sandeepp2016 <sandeeppatelsgsits@gmail.com>
2016-07-26 11:00:30 +0530

    auth file removed

--- microservices/http-server/rest-server/api/auth/twitter.auth.config.js
+++ microservices/http-server/rest-server/api/auth/twitter.auth.config.js
@@ -1,10 +1,8 @@
 // var redirectPort = process.env.REDIRECT_PORT || 8001;
 // var redirectHost = process.env.REDIRECT_HOST || "localhost";
-var redirectPort = 8001;
-var redirectHost = '192.168.99.100';
 
 exports = module.exports =  {
-  consumerKey: '0jyQWlJr83HiPcmPyHkBEzgGR',
-  consumerSecret:'kAbzWjtRn0gS9VdIyNzL1PA6NxrYUZ6Y8MCc6eVtGJO8F02WP7',
-  callback:'http://'+redirectHost+':'+redirectPort+'/api/v1/auth/twitter/success'
+  consumerKey: '',
+  consumerSecret:'',
+  callback:'callback url'
 }
--- microservices/http-server/rest-server/api/timeline/twitter.auth.config.js
+++ microservices/http-server/rest-server/api/timeline/twitter.auth.config.js
@@ -1,4 +1,4 @@
 exports = module.exports =  {
-                           consumer_key: '0jyQWlJr83HiPcmPyHkBEzgGR',
-                           consumer_secret:'kAbzWjtRn0gS9VdIyNzL1PA6NxrYUZ6Y8MCc6eVtGJO8F02WP7',
+                           consumer_key: ,
+                           consumer_secret:,
                            }
